e646f9a908f74c00c2aa756914ebfa57f0534923
kristen <kafkaloff@berkeley.edu>
2018-11-10 14:17:16 -0800

    optimal solution with [size] number of students, run in terminal: python3 outputs.py <size>

--- outputs.py
+++ outputs.py
@@ -1,10 +1,13 @@
 import networkx as nx
 import os
 import matplotlib.pyplot as plt
+import sys
 
 small = 7
 medium = 22
 large = 31
+
+size = None
 
 def small_output():
     
@@ -33,12 +36,37 @@ def small_output():
     nx.draw(graph, pos, node_size = 25, font_size = 8, with_labels=True)
     plt.show()
 
+def output(size):
+    
+    graph = nx.Graph()
+    num_students = size * size
+    num_buses = size
 
+    students = []
+    for i in range(num_students):
+        students.append(i + 1)
 
-def main():
-    small_output()
+    graph.add_nodes_from(students)
+
+    student = 1
+    while student <= num_students:
+        for i in range(student, student + size):
+            for k in range(student, student + size):
+                if i != k:
+                    graph.add_edge(i, k)
+        student += size
+
+    # print(list(graph.nodes))
+
+    # play around with k and iterations - more iterations = centered around component, k = optimal node dist
+    pos=nx.spring_layout(graph, k = .9, iterations = 70)
+    nx.draw(graph, pos, node_size = 25, font_size = 8, with_labels=True)
+    plt.show()
 
 if __name__ == '__main__':
-    main()
+    size = int(sys.argv[1])
+    if size != None:
+        output(size)
+    # small_output()
 
 
